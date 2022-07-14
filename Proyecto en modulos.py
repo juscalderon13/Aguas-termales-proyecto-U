9d6bfd158b3474f2f9ca1f57cc458b420c414c98
juscalderon29 <juscalderon13@gmail.com>
2022-07-13 18:48:46 -0600

    ya puesto el programa principal

--- Proyecto en modulos.py
+++ Proyecto en modulos.py
@@ -45,11 +45,11 @@ def Login():
             print("")
             print(" "*28, "INICIO DE SESION EXITOSO!")
             print("")
-            print("-"*80)
             acceso = 1
     return acceso
 
 def MainMenu():
+    print("-"*80)
     print("-"*32, "MENU PRINCIPAL", "-"*32)
     print("-"*80)
     print('')
@@ -232,8 +232,8 @@ porfavor seleccione una de las siguientes opciones: """)
 
     print('''Ingrese el numero al que desea ir:
 
-[1] - Volver al menu de registros y agendas.
-[2] - Volver al menu principal.\n''')
+[1] - Realizar otra reservacion.
+[2] - Volver al menu de Registros y Agendas.\n''')
     print('Desea ir al:')
     subModAgenda = int(input())
     valores = [1,2]
@@ -249,9 +249,268 @@ porfavor seleccione una de las siguientes opciones: """)
          subModAgenda = int(input())
     return subModAgenda
 
+def ModuloVentas():
+    print("")
+    print("-"*80)
+    print("-"*36, "VENTAS", "-"*36)
+    print("-"*80)
+    print('')
+    nombre = input("Ingresa el nombre del cliente para la factura: ")
+    cafe = 1500
+    llavero = 1000
+    gorra = 5000
+    chocolate = 2500
+    iva = 0.13
+    descuento = 10
+
+    #Mostrar menú disponible para compras.
+    print("\nElige el producto que deseas comprar")
+    print("""
+[1] = Café nacional
+[2] = Llavero típico
+[3] = Gorra con mensaje nacional
+[4] = Chocolate de café nacional""")
+
+    #Operaciones.
+
+
+    #Se selecciona los productos deseados.
+    eleccion = input("\nSelecciona el producto que desees: ")
+    if eleccion == "1":
+        print("Has seleccionado Café nacional")
+        cantidad = int(input("Cantidad de paquetes: "))
+        total = print("\nEl total sería: ", cantidad * cafe, "colones")
+    elif eleccion == "2":
+        print("Has seleccionado Llavero típico")
+        cantidad = int(input("Cantidad de llaveros: "))
+        total2 = print("\nEl total sería: ", cantidad * llavero, "colones")
+    elif eleccion == "3":
+        print("Has seleccionado Gorras con mensaje nacional")
+        cantidad = int(input("Cantidad de gorras: "))
+        total3 = print("\nEl total sería: ", cantidad * gorra, "colones")
+    elif eleccion == "4":
+        print("Has seleccionado Chocolates de café nacional")
+        cantidad = int(input("Cantidad de chocolates: "))
+        total = print("\nEl total sería: ", cantidad * chocolate, "colones")
+    else:
+        print("Opción inexistente, elige una opción válida")
+    
+    #Factura final
+    print("\nFactura a nombre de: ", nombre)
+    print("Los productos que lleva son: ")
+    print("El precio total es: ")
+    print("El IVA correspondiente es del 13%")
+    print("El precio de los productos con el IVA es de: ")
+    print("El descuento a aplicar es de: ", descuento, "%")
+    print("El precio total con el descuento es: ")
+    print("El precio final de los productos seria: ")
+    print('')
+    print('''Ingrese el numero al que desea ir:
+
+[1] - Realizar otra venta.
+[2] - Volver al menu principal. \n''')
+    print('Desea ir al:')
+    subModVentas = int(input())
+    valores = [1,2]
+    while not subModVentas in valores:
+        print("""
+                 VALOR INVALIDO!
+porfavor seleccione una de las siguientes opciones: """)
+        print("""
+[1] - Realizar otra venta.
+[2] - Volver al menu principal.
+""")
+        print('Desea ir al:')
+        subModVentas = int(input())
+    return subModVentas
+
+def ModuloHistorial():
+    print("")
+    print("-"*80)
+    print("-"*35, "HISTORIAL", "-"*34)
+    print("-"*80)
+    print('')
+    print("""Ingrese el numero del modulo al que desea ir:
+
+[1] - Listado de aforo por día según el tipo de persona que ingreso.
+[2] - citas programadas de acuerdo a los clientes registrados.
+[3] - Listado de productos, segun articulos facturados.
+[4] - Volver al Menu Principal.
+""")
+    #variable submodulo Historial para seleccionar opciones en modulo historial
+    print("Desea ir al: ")
+    subModHis = int(input())
+    valores = [1,2,3,4]
+    while not subModHis in valores:
+        print("""
+                 VALOR INVALIDO!
+porfavor seleccione una de las siguientes opciones: """)
+        print("""
+[1] - Listado de aforo por día según el tipo de persona que ingreso.
+[2] - citas programadas de acuerdo a los clientes registrados.
+[3] - Listado de productos, segun articulos facturados.
+[4] - Volver al Menu Principal.
+""")
+        print('Desea ir al:')
+        subModHis = int(input())
+    return subModHis
+
+def SubModulAforo():
+    nino = 2
+    adulto = 9
+    adultoMa = 15
+    #total de aforo por dia
+    totalAfor = nino + adulto + adultoMa
+    #Nombre de persona
+    name = "Elvis Tec"
+    #fecha valida por el momento
+    fecha = "15/06/2022"
+    hora = '12:30 p.m.'
+    numeroReser = "#3246"
+    estado = "Pago"
+    print("")
+    print("-"*80)
+    print("-"*32, "LISTA DE AFORO", "-"*32)
+    print("-"*80)
+    print('')
+    print("Ingrese la fecha que desea ver el historial en el siguiente formato \n (DD/MM/AAAA): \n")
+    fechaInp = input()
+    if fecha != fechaInp:
+        print('fecha invalida por favor intente de nuevo')
+    else:
+        print(" "*20,f"Historial del dia {fechaInp}. \n")
+        print(f'El aforo fue de {totalAfor} personas entre los siguientes tipos: \n')
+        print(f'niños: {nino}')
+        print(f'Adultos: {adulto}')
+        print(f'Adultos mayores: {adultoMa}')
+        enter = input()
+        print('')
+        print('''Digite el numero al que desea ir:
+
+[1] - Volver al menu de Historial.
+[2] - Volver al menu principal. \n''')
+        print('Desea ir al:')
+        subModAforo = int(input())
+        valores = [1,2]
+        while not subModAforo in valores:
+            print("""
+                 VALOR INVALIDO!
+porfavor seleccione una de las siguientes opciones: """)
+            print("""
+[1] - Volver al menu de Historial.
+[2] - Volver al menu principal.
+""")
+            print('Desea ir al:')
+            subModAforo = int(input())
+    return subModAforo
+
+def SubModulCitas():
+    nino = 2
+    adulto = 9
+    adultoMa = 15
+    #total de aforo por dia
+    totalAfor = nino + adulto + adultoMa
+    #Nombre de persona
+    name = "Elvis Tec"
+    #fecha valida por el momento
+    fecha = "15/06/2022"
+    hora = '12:30 p.m.'
+    numeroReser = "#3246"
+    estado = "Pago"
+    print("")
+    print("-"*80)
+    print("-"*30, "CITAS PROGRAMADAS", "-"*31)
+    print("-"*80)
+    print('')
+    print('Ingrese la fecha para ver las reservaciones de ese dia \n (DD/MM/AAAA): \n')
+    fechaInp = input()
+    if fecha != fechaInp:
+        print('fecha invalida por favor intente de nuevo')
+    else:
+        print(" "*20,f"Reservaciones del dia {fechaInp}. \n")
+        print(f'[Reservacion]         [nombre]          [Hora]        [Estado]')
+        print(f" {numeroReser}                 {name}        {hora}     {estado}")
+        print('')
+        enter = input("Presione ENTER para continuar")
+        print('''Digite el numero al que desea ir:
+
+[1] - Volver al menu de Historial.
+[2] - Volver al menu principal. \n''')
+        print('Desea ir al:')
+        subModCitas = int(input())
+        valores = [1,2]
+        while not subModCitas in valores:
+            print("""
+                 VALOR INVALIDO!
+porfavor seleccione una de las siguientes opciones: """)
+            print("""
+[1] - Volver al menu de Historial.
+[2] - Volver al menu principal.
+""")
+            print('Desea ir al:')
+            subModCitas = int(input())
+    return subModCitas
+
+def SubModuloLisProduc():
+    print("")
+    print("-"*80)
+    print("-"*30, "LISTA DE PRODUCTOS", "-"*30)
+    print("-"*80)
+    print('')
+    print('[Producto]        [Vendidos]       [stock]        [Precio de venta]')
+    print(" Crema facial         7              12                  $45")
+    print(' Crema humectante     4              15                  $35')
+    print('')
+    print('''Digite el numero al que desea ir:
+
+[1] - Volver al menu de Historial.
+[2] - Volver al menu principal. \n''')
+    print('Desea ir al:')
+    subModLista = int(input())
+    valores = [1,2]
+    while not subModLista in valores:
+            print("""
+                 VALOR INVALIDO!
+porfavor seleccione una de las siguientes opciones: """)
+            print("""
+[1] - Volver al menu de Historial.
+[2] - Volver al menu principal.
+""")
+            print('Desea ir al:')
+            subModLista = int(input())
+    return subModLista
+
+def ModuloExit():
+    print("")
+    print("-"*80)
+    print("-"*36, "SALIR", "-"*37)
+    print("-"*80)
+    print('')
+    print('Esta saliendo de la aplicacion digite: ')
+    print("""
+[1] - Para salir de la aplicacion y cerrar sesion. 
+[2] - Cancelar y volver al menu principal
+""")
+    #variable submodulo salida para seleccionar opciones en modulo salida
+    print('Desea cerrar sesion? :')
+    subModSalir = int(input())
+    valores = [1,2]
+    while not subModSalir in valores:
+        print("""
+                 VALOR INVALIDO!
+porfavor seleccione una de las siguientes opciones: """)
+        print("""
+[1] - Para salir de la aplicacion y cerrar sesion. 
+[2] - Cancelar y volver al menu principal
+""")
+        print('Desea ir al:')
+        subModSalir = int(input())
+    return subModSalir
+
 acceso = Login()
 while acceso == 1:
     moduloPrincipal = MainMenu()
+    
     if moduloPrincipal == 1:
         while moduloPrincipal == 1:
             x = ModuloRegisyAgen()
@@ -263,5 +522,34 @@ while acceso == 1:
                 z = SubModulAgenVisit()
                 while z == 1:
                     z = SubModulAgenVisit()
+            else:
+                moduloPrincipal = 0
+                
+    elif moduloPrincipal == 2:
+        while moduloPrincipal == 2:
+            x1 = ModuloVentas()
+            if x1 == 2:
+                moduloPrincipal = 0
+                
+    elif moduloPrincipal == 3:
+        while moduloPrincipal == 3:
+            x2 = ModuloHistorial()
+            if x2 == 1:
+                y2 = SubModulAforo()
+                while y == 1:
+                    y2 = SubModulAforo()
+            elif x2 == 2:
+                z2 = SubModulCitas()
+                while z2 == 2:
+                    z2 = SubModulCitas()
+            elif x2 == 3:
+                a2 = SubModuloLisProduc()
+                while a2 == 3:
+                    a2 = SubModuloLisProduc()
+            else:
+                moduloPrincipal = 0
                     
-            
+    elif moduloPrincipal == 4:
+        salida = ModuloExit()
+        if salida == 1:
+            acceso = 0
